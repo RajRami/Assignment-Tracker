d11282ab67bda2ac783e444f0311ab378fc479e5
Raj <200489571@student.georgianc.on.ca>
2022-11-24 18:48:06 -0500

    Perform create operation, saved the data and populate the index view

--- controllers/assignments.js
+++ controllers/assignments.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+//Import the appoitment model class
+const Assignments = require('../models/assignmentModel')
+
 //GET: /assignments => Display the list of assignments
 router.get('/', (req, res) => {
-    res.render('assignments/index', {
-        title: 'Assignments'
+    Assignments.find((err, assignments) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('assignments/index', {
+                title: 'Assignments',
+                assignments: assignments
+            })
+        }
     })
 })
 
@@ -12,6 +23,20 @@ router.get('/', (req, res) => {
 router.get('/create', (req, res) => {
     res.render('assignments/create', {
         title: 'Add an assignments'
+    })
+})
+
+//POST: //POST: /assignments/create => Process the form submission
+router.post('/create', (req, res) => {
+    //Add a new assignments based on the response
+    Assignments.create(req.body, (err, newAssignment) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            // res.json(newAssignment)
+            res.redirect('/assignments')
+        }
     })
 })
 
