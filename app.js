0bb05627a135780caa78435c062235dd0ab80074
Raj <200489571@student.georgianc.on.ca>
2022-11-27 00:04:35 -0500

    Google auth config

--- app.js
+++ app.js
@@ -35,9 +35,26 @@ passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
+//Google auth config
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+},
+  (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({oauthId: profile.id}, {
+      username: profile.displayName,
+      oauthPrvider: 'Google',
+      created: Date.now()
+    }, (err, user) => {
+      return done(err, user)
+    })
+  }
+))
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL).then(() => {
   console.log('Connected to Database')
 }).catch(() => {
