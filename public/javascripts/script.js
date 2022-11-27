0feb2be5f5c4041f4facc1a4ef03cfb3323cd56c
Raj <200489571@student.georgianc.on.ca>
2022-11-26 23:36:15 -0500

    Implement password check on register and added goole keys

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,21 @@
 function confirmDelete(){
     return confirm("Are you sure you want to delete this item?")
 }
+
+//Password Check
+function comparePasswords(){
+    let password = document.getElementById('password').value
+    let confirm = document.getElementById('confirm').value
+    let msgLabel = document.getElementById('msgLabel')
+
+    if(password != confirm){
+        msgLabel.innerText = "Password does not match"
+        msgLabel.className = "text-danger"
+        return false
+    }
+    else{
+        msgLabel.innerText = ""
+        msgLabel.className = ""
+        return true
+    }
+}
